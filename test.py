2f3c16534a944ebd1e335c0820f8dcbcc4ffcd38
Colton Mikes <colton@coltonmikes.com>
2021-09-15 13:24:18 -0400

    Added a third test vector and second print statement.

--- test.py
+++ test.py
@@ -9,8 +9,8 @@ def add_vectors(vector_a,vector_b):
 
 test_vector_01 = [1,2,3]
 test_vector_02 = [4,3,2]
-
+test_vector_03 = [1,6,3]
 
 print(add_vectors(test_vector_01,test_vector_02))
 
-
+print(add_vector(test_vector_03,test_vector_01))
